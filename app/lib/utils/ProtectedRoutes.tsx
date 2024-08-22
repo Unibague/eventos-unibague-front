99a73db8aa3462d4538fdeec907b9e0fa242c8b4
JEstebanGnz <juanes01.gm@outlook.com>
2024-08-22 17:05:11 -0500

    test

--- app/lib/utils/ProtectedRoutes.tsx
+++ app/lib/utils/ProtectedRoutes.tsx
@@ -10,95 +10,97 @@ import { Role } from '../types';
 import { getEvent } from './api/getEvent';
 
 // Custom hook for notification
-const useNotification = () => {
-  const [notification, setNotification] = useState({
-    open: false,
-    message: '',
-  });
-  const showNotification = (message: string) =>
-    setNotification({ open: true, message });
-  const closeNotification = () => setNotification({ open: false, message: '' });
-  return { notification, showNotification, closeNotification };
-};
+// const useNotification = () => {
+//   const [notification, setNotification] = useState({
+//     open: false,
+//     message: '',
+//   });
+//   const showNotification = (message: string) =>
+//     setNotification({ open: true, message });
+//   const closeNotification = () => setNotification({ open: false, message: '' });
+//   return { notification, showNotification, closeNotification };
+// };
 
-const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const [isVerifying, setIsVerifying] = useState(true);
-  const { notification, showNotification, closeNotification } =
-    useNotification();
-  const { data: session, status } = useSession();
+ const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
+//   const router = useRouter();
+//   const pathname = usePathname();
+//   const [isVerifying, setIsVerifying] = useState(true);
+//   const { notification, showNotification, closeNotification } =
+//     useNotification();
+//   const { data: session, status } = useSession();
 
-  const checkEventAccess = async (eventId: number) => {
-    const event = await getEvent(eventId);
-    if (!event?.restrictedAccess) return true;
+//   const checkEventAccess = async (eventId: number) => {
+//     const event = await getEvent(eventId);
+//     if (!event?.restrictedAccess) return true;
 
-    if (!session?.user) {
-      router.replace('/auth/login');
-      return false;
-    }
-    if (!session.user.id) return false;
-    return userCanAccessEvent(eventId, session.user.accessibleEvents);
-  };
+//     if (!session?.user) {
+//       router.replace('/auth/login');
+//       return false;
+//     }
+//     if (!session.user.id) return false;
+//     return userCanAccessEvent(eventId, session.user.accessibleEvents);
+//   };
 
-  const checkAdminAccess = () => {
-    if (!session?.user) return false;
-    return (
-      session.user.roles?.some((role: Role) => role.name === 'admin') ?? false
-    );
-  };
+//   const checkAdminAccess = () => {
+//     if (!session?.user) return false;
+//     return (
+//       session.user.roles?.some((role: Role) => role.name === 'admin') ?? false
+//     );
+//   };
 
-  useEffect(() => {
-    const checkAccess = async () => {
-      const eventRoutes = /^\/event\/(\d+)\/(.+)/;
-      const adminRoutes = /^\/admin/;
-      if (eventRoutes.test(pathname)) {
-        const match = pathname.match(eventRoutes);
-        if (match) {
-          const eventId = parseInt(match[1]);
-          const canAccess = await checkEventAccess(eventId);
-          if (!canAccess) {
-            showNotification('No tienes permiso para acceder a esta página');
-            router.replace('/landing');
-            return;
-          }
-        }
-      } else if (adminRoutes.test(pathname)) {
-        if (!checkAdminAccess()) {
-          showNotification('No tienes permisos para acceder a esta página');
-          router.replace('/landing');
-          return;
-        }
-      }
-      setIsVerifying(false);
-    };
+//   useEffect(() => {
+//     const checkAccess = async () => {
+//       const eventRoutes = /^\/event\/(\d+)\/(.+)/;
+//       const adminRoutes = /^\/admin/;
+//       if (eventRoutes.test(pathname)) {
+//         const match = pathname.match(eventRoutes);
+//         if (match) {
+//           const eventId = parseInt(match[1]);
+//           const canAccess = await checkEventAccess(eventId);
+//           if (!canAccess) {
+//             showNotification('No tienes permiso para acceder a esta página');
+//             router.replace('/landing');
+//             return;
+//           }
+//         }
+//       } else if (adminRoutes.test(pathname)) {
+//         if (!checkAdminAccess()) {
+//           showNotification('No tienes permisos para acceder a esta página');
+//           router.replace('/landing');
+//           return;
+//         }
+//       }
+//       setIsVerifying(false);
+//     };
 
-    // Early return if session is null or session.user.id is undefined
-    if (session === null || session.user?.id === undefined) {
-      setIsVerifying(false); // Set to false to stop showing loading screen
-      return;
-    }
+//     // Early return if session is null or session.user.id is undefined
+//     if (session === null || session.user?.id === undefined) {
+//       setIsVerifying(false); // Set to false to stop showing loading screen
+//       return;
+//     }
 
-    //Only do this if the user is not logged or there's already a logged user!
-    if (session == null || session.user?.id !== undefined) {
-      checkAccess();
-    }
+//     //Only do this if the user is not logged or there's already a logged user!
+//     if (session == null || session.user?.id !== undefined) {
+//       checkAccess();
+//     }
       
-  }, [pathname, router, session]);
+//   }, [pathname, router, session]);
 
-  if (isVerifying) return <LoadingScreen />;
+//   if (isVerifying) return <LoadingScreen />;
 
-  return (
-    <>
-      {children}
-      <Notification
-        message={notification.message}
-        open={notification.open}
-        onClose={closeNotification}
-        snackbarColor="red"
-      />
-    </>
-  );
+   return (
+     <>
+
+     </>)
+//       {children}
+//       <Notification
+//         message={notification.message}
+//         open={notification.open}
+//         onClose={closeNotification}
+//         snackbarColor="red"
+//       />
+//     </>
+//   );
 };
 
 export default ProtectedRoutes;
