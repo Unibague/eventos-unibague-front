095e2223ed82408ed9f0db07d6926f507e818b19
JEstebanGnz <juanes01.gm@outlook.com>
2024-09-10 09:23:59 -0500

    additional padding added

--- app/event/[id]/layout.tsx
+++ app/event/[id]/layout.tsx
@@ -67,7 +67,7 @@ export default async function EventLayout({ children, params }: EventLayoutProps
           flexGrow: 1,
           display: 'flex',
           flexDirection: 'column',
-          paddingBottom: '5vh', // Adjust this to the height of your footer if needed
+          paddingBottom: '8vh', // Adjust this to the height of your footer if needed
         }}
       >
         {children}
